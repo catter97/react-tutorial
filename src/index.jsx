33b62a9832eb54976f8913d2c57fd3eb27d6c38b
chang hyup Cho <catter97@gmail.com>
2018-12-23 07:48:13 +0900

    Separate TodoList from index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import TodoList from './components/TodoList.jsx';
 
 const todoData = ['Running', 'Homework'];
 
-const TodoList = ({ todos }) => (
-  <ul>
-    {todos.map(todo => <li>{todo}</li>)}
-  </ul>
-);
-
-TodoList.propTypes = {
-  todos: PropTypes.string.isRequired,
-};
-
 ReactDOM.render(
   <div>
+    <h1>Todo</h1>
     <TodoList todos={todoData} />
   </div>,
   document.getElementById('root'),
