5c09ecfb857ad2dc5a30fe8c76457fd243790258
chang hyup Cho <catter97@gmail.com>
2018-12-23 07:19:31 +0900

    TodoList as a pure function

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import _ from 'lodash';
+import PropTypes from 'prop-types';
+
+const todoData = ['Running', 'Homework'];
+
+const TodoList = ({ todos }) => (
+  <ul>
+    {todos.map(todo => <li>{todo}</li>)}
+  </ul>
+);
+
+TodoList.propTypes = {
+  todos: PropTypes.string.isRequired,
+};
 
 ReactDOM.render(
   <div>
-    <h1>Todo</h1>
-    <ul>
-      <li>Running</li>
-      <li>Homework</li>
-    </ul>
+    <TodoList todos={todoData} />
   </div>,
   document.getElementById('root'),
 );
