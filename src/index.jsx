f9666a4112dedd8e83af2976dc69b40ebf1d80a7
chang hyup Cho <catter97@gmail.com>
2018-12-23 08:38:17 +0900

    Separate App from index.jsx.

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from 'components/TodoList';
-
-const todoData = [{
-  id: 1,
-  text: 'Running',
-  completed: false,
-}, {
-  id: 2,
-  text: 'Homework',
-  completed: true,
-}];
-
-const toggleCompleted = (id) => {
-  const todo = todoData.find(t => t.id === id);
-  todo.completed = !todo.completed;
-};
+import App from 'components/App';
 
 ReactDOM.render(
-  <div>
-    <h1>Todo</h1>
-    <TodoList todos={todoData} onClick={toggleCompleted} />
-  </div>,
+  <App />,
   document.getElementById('root'),
 );
